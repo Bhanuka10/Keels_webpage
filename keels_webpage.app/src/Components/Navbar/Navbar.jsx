f8867f594e7f30cdca94d39f17ac2d308375a0b3
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-19 21:32:10 +0530

    create a functions

--- keels_webpage.app/src/Components/Navbar/Navbar.jsx
+++ keels_webpage.app/src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import KeellsLogo from '../../assets/keells-logo-png_seeklogo-431833.png'; // Ensure correct path
 import serch from '../../assets/icons8-search-50.png';
 import basket from '../../assets/icons8-basket-64.png';
 
 function Navbar() {
+    const[menu,setmenu] = useState("Menu");
   return (
     <div className='navbar'>
       <img className='kells_logo' src={KeellsLogo} alt="Keells Logo" />
       <div className='navbar-men'>
         <ul>
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile App</li>
-            <li>Contact Us</li>
+            <li className={menu==="Home"?"active":""}>Home</li>
+            <li className={menu==="Menu"?"active":""}>Menu</li>
+            <li className={menu==="Mobile App"?"active":""}>Mobile App</li>
+            <li className={menu==="Contact Us"?"active":""}>Contact Us</li>
         </ul>
       </div>
       <div className='navbar-right'>
@@ -21,8 +22,8 @@ function Navbar() {
         <div className='navbar-serch-icon'>
           <img src={basket} alt="Basket Icon" />
           <div className='dot'></div>
-        </div>
-        <button> Sign IN</button>
+        </div  >
+        <button className='sign_btn'>  Sign IN</button>
       </div>
     </div>
   );
