c5874062253f8a7fa46ecf59d99568cab0d58770
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-19 21:41:04 +0530

    activate function

--- keels_webpage.app/src/Components/Navbar/Navbar.jsx
+++ keels_webpage.app/src/Components/Navbar/Navbar.jsx
@@ -5,16 +5,16 @@ import serch from '../../assets/icons8-search-50.png';
 import basket from '../../assets/icons8-basket-64.png';
 
 function Navbar() {
-    const[menu,setmenu] = useState("Menu");
+    const[menu,setmenu] = useState("menu");
   return (
     <div className='navbar'>
       <img className='kells_logo' src={KeellsLogo} alt="Keells Logo" />
       <div className='navbar-men'>
         <ul>
-            <li className={menu==="Home"?"active":""}>Home</li>
-            <li className={menu==="Menu"?"active":""}>Menu</li>
-            <li className={menu==="Mobile App"?"active":""}>Mobile App</li>
-            <li className={menu==="Contact Us"?"active":""}>Contact Us</li>
+            <li onClick={()=> setmenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+            <li onClick={()=> setmenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+            <li onClick={()=> setmenu("Mobile App")} className={menu==="Mobile App"?"active":""}>Mobile App</li>
+            <li onClick={()=> setmenu("Contact Us")} className={menu==="Contact Us"?"active":""}>Contact Us</li>
         </ul>
       </div>
       <div className='navbar-right'>
