7f99daaceb624bd3c9087d151ee2b4d8bffabe91
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-24 02:41:02 +0530

    update on web pages

--- keels_webpage.app/src/Components/Header/Header.jsx
+++ keels_webpage.app/src/Components/Header/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='header-conteints'>
+        <h2>Freshness, Value, Convenience – Keells Delivers!</h2>
+        <p>
+        Keells is your go-to supermarket for fresh products, great value, and convenience. We offer high-quality groceries, fresh produce, and essentials while ensuring a seamless shopping experience. </p>
+        <button>View menu</button>
+
+      </div>
+
+    </div>
   )
 }
 
