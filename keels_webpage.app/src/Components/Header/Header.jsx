5d77f5a6f2cfe0e781e0a9602e7ff9f315375ba1
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-24 13:18:54 +0530

    complete the header part

--- keels_webpage.app/src/Components/Header/Header.jsx
+++ keels_webpage.app/src/Components/Header/Header.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Header.css'; // Ensure the CSS file is imported
 
 const Header = () => {
   return (
     <div className='header'>
-      <div className='header-conteints'>
-        <h2>Freshness, Value, Convenience – Keells Delivers!</h2>
+      <div className='header-contents'>
+        <h1>Keels</h1>
+        <h2>Freshness, Value, Convenience Keells Delivers!</h2>
+
         <p>
-        Keells is your go-to supermarket for fresh products, great value, and convenience. We offer high-quality groceries, fresh produce, and essentials while ensuring a seamless shopping experience. </p>
+          Keells is your go-to supermarket for fresh products, great value, and convenience. We offer high-quality groceries, fresh produce, and essentials while ensuring a seamless shopping experience.
+        </p>
         <button>View menu</button>
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
