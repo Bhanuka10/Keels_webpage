adcc734bb5b361feeaddd1a62f337a95737807cf
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-31 23:29:25 +0530

    keels photo add and update the image

--- keels_webpage.app/src/Components/Exploremenu/Exploremenu.jsx
+++ keels_webpage.app/src/Components/Exploremenu/Exploremenu.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import './Exploremenu.css'
+import React from 'react';
+import './Exploremenu.css';
+import { menu_list } from '../../assets/frontend_assets/assets';
+
 const Exploremenu = () => {
   return (
-    <div>Exploremenu</div>
-  )
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore Our Menu</h1>
+      <p className='explore-menu-text'>Discover an endless variety of fresh, high-quality products—all in one place, just for you at Keells!</p>
+      <div className='explore-menu-list'>
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className='explore-menu-list-item'>
+              <img src={item.menu_image }alt="" />
+              <p>{item.menu_name}</p>
+
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Exploremenu
+export default Exploremenu;
--- keels_webpage.app/src/Pages/Home/Home.jsx
+++ keels_webpage.app/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css';
 import Header from '../../Components/Header/Header';
+import Exploremenu from '../../Components/Exploremenu/Exploremenu';
 const Home = () => {
   return (
     <div>
       <Header />
+      <Exploremenu />
     </div>
   )
 }
