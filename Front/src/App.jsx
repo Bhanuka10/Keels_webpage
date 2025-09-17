b79441a1aff31ea81d028a75b8a373a4f9795bed
Yasithb <pybpeiris@std.foc.sab.ac.lk>
2025-09-18 02:00:26 +0530

    Create footer

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <div className="app-container">
+        <Navbar />
+        <Footer />
+      </div>
     </>
   )
 }
