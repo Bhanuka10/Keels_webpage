47fe49b323c8d01f00030cfe7a08825c2fc32166
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-09-18 23:10:11 +0530

    edit the nav bar

--- Front/src/Components/Navigation/Navigation.jsx
+++ Front/src/Components/Navigation/Navigation.jsx
@@ -1,48 +1,51 @@
 import React from 'react';
 import './Navigation.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Needed for sidebar toggle
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function Navigation() {
   return (
-    <div>
-        <div className='navbarr'>
-            <div className='"navbar navbar-expand-lg navbar-dark bg-dark"'>
-                <button
-  className="btn btn-outline-secondary"
-  type="button"
-  data-bs-toggle="offcanvas"
-  data-bs-target="#sidebar"
-  aria-controls="sidebar"
->
-  <span className="navbar-toggler-icon" style={{ width: '15px', height: '10px'  }}></span>
-</button>
+<div className='nav_bar'>
+  <div className='nav_sidebar'>
+    <i className="bi bi-list" style={{ fontSize: '20px', cursor: 'pointer' }}></i>
 
-            </div>
-            <div className='navbar-content'>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <p>|</p>
-                    <li><a href="#">About Us</a></li>
-                    <p>|</p>
-                    <li><a href="#">Services</a></li>
-                    <p>|</p>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-            <div className='login-part'>
-                <div><a href="#">Login</a></div>
-                <div><a href="#">Register</a></div>
-                
-            </div>
-            <div>
-                    <i className="bi bi-cart" style={{ fontSize: '25px', paddingLeft: '0', fontWeight: 'bold' }}></i> 
-                </div>
 
-        </div>
-    </div>
+  </div>
+  <div className='searchbar'>
+    <input type='text' placeholder='Search here...' />
+    <i className="bi bi-search" style={{ fontSize: '16px', cursor: 'pointer' }}></i>
+
+  </div>
+  <div className="nav_content">
+    
+      <ul>
+        <li><a href="#">Home</a></li>
+        <p>|</p>
+        <li><a href="#">About</a></li>
+        <p>|</p>
+        <li><a href="#">Services</a></li>
+        <p>|</p>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    
+  </div>
+  <div className='nav_user'>
+  <div className="login">
+    <a href="#">Login</a>
+    <a href="#">Sign Up</a>
+  </div>
+<div className="cart">
+
+  <i className="bi bi-cart"></i> 
+
+
+  </div>
+  </div>
+  
+
+</div>
 
   );
 }
